a3cd2d54c04730f0a318ff4981a68166a50f1c93
keyan <keyan.wei@vhall.com>
2016-10-21 12:38:58 +0800

    no message

---
+++ ToolsSet/ToolsSet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ToolsSet
+//
+//  Created by keyan on 2016/10/21.
+//  Copyright © 2016年 ky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ToolsSet/ToolsSet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToolsSet
+//
+//  Created by keyan on 2016/10/21.
+//  Copyright © 2016年 ky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
